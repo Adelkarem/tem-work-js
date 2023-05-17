c3ca2028d4b3cbe614603e39902c6158eae8c7d2
Adelkarem <89422696+Adelkarem@users.noreply.github.com>
2023-05-17 10:16:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 
+ $('.counter').counterUp({
+   delay: 10,
+   time: 1000
+});
 
  // contact
 // map
@@ -98,58 +102,7 @@ function toggleDarkMode() {
 
 
 
-// / clock and date///
-setInterval(()=>{
-  const time = document.querySelector(".display #time");
-  let date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  let day_night = "AM";
-  if(hours > 12){
-    day_night = "PM";
-    hours = hours - 12;
-  }
-  if(seconds < 10){
-    seconds = "0" + seconds;
-  }
-  if(minutes < 10){
-    minutes = "0" + minutes;
-  }
-  if(hours < 10){
-    hours = "0" + hours;
-  }
-  time.textContent = hours + ":" + minutes + ":" + seconds + " "+ day_night;
-});
-const month = document.querySelector('.month');
-const day = document.querySelector('.day');
-const year = document.querySelector('.year');
 
-function setDate() {
-const now = new Date();
-const mm = now.getMonth();
-const dd = now.getDate();
-const yyyy = now.getFullYear();
-const secs = now.getSeconds();
-const mins = now.getMinutes();
-const hrs = now.getHours();
-const monthName = [
-  'January','February','March','April',
-  'May','June','July','August','September',
-  'October','November','December'
-];
-
-  month.innerHTML = monthName[mm];
-day.innerHTML = dd;
-year.innerHTML = yyyy;
-}
-
-setInterval(setDate,1000);
-
-// end clock and date////
+ 
 
 
-// $('.counter').counterUp({
-//   delay: 10,
-//   time: 1000
-// });
